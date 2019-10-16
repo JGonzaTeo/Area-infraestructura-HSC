27ab266b51cb8751f43cafa2906626ced402640a
Allan Letona <aletonam@miumg.edu.gt>
2019-10-15 21:34:22 -0600

    Script con bd navegador y seguridad

--- ProyectoGeneral.sql
+++ ProyectoGeneral.sql
@@ -1885,6 +1885,269 @@ UNLOCK TABLES;
 --
 -- Dumping events for database 'proyectogeneral'
 --
+-- --------------------------------------------- NAVEGADOR ---------------------------------
+
+CREATE TABLE `ayuda` (
+  `Id_ayuda` int(11) NOT NULL,
+  `Ruta` text COLLATE utf8_unicode_ci NOT NULL,
+  `indice` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- -------------------------------------------- SEGURIDAD --------------------------------------
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procedimientoLogin` (IN `usuario` VARCHAR(25), IN `clave` VARCHAR(25))  NO SQL
+Select tbl_usuario.PK_id_Usuario FROM tbl_usuario where tbl_usuario.PK_id_Usuario = usuario and tbl_usuario.password_usuario = MD5(clave) AND tbl_usuario.estado_usuario=1$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_aplicacion`
+--
+
+CREATE TABLE `tbl_aplicacion` (
+  `PK_id_aplicacion` int(11) NOT NULL,
+  `PK_id_modulo` int(11) NOT NULL,
+  `nombre_aplicacion` varchar(45) DEFAULT NULL,
+  `descripcion_aplicacion` varchar(200) DEFAULT NULL,
+  `estado_aplicacion` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_bitacora`
+--
+
+CREATE TABLE `tbl_bitacora` (
+  `PK_id_bitacora` int(11) NOT NULL,
+  `PK_id_usuario` varchar(25) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `host` varchar(45) DEFAULT NULL,
+  `ip` varchar(25) DEFAULT NULL,
+  `accion` varchar(50) DEFAULT NULL,
+  `tabla` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_modulo`
+--
+
+CREATE TABLE `tbl_modulo` (
+  `PK_id_Modulo` int(11) NOT NULL,
+  `nombre_modulo` varchar(45) DEFAULT NULL,
+  `descripcion_modulo` varchar(200) DEFAULT NULL,
+  `estado_modulo` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_perfil_detalle`
+--
+
+CREATE TABLE `tbl_perfil_detalle` (
+  `PK_id_perfil` int(11) NOT NULL,
+  `PK_id_aplicacion` int(11) NOT NULL,
+  `ingresar` tinyint(4) DEFAULT NULL,
+  `consultar` tinyint(4) DEFAULT NULL,
+  `modificar` tinyint(4) DEFAULT NULL,
+  `eliminar` tinyint(4) DEFAULT NULL,
+  `imprimir` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_perfil_encabezado`
+--
+
+CREATE TABLE `tbl_perfil_encabezado` (
+  `PK_id_perfil` int(11) NOT NULL,
+  `nombre_perfil` varchar(45) DEFAULT NULL,
+  `descripcion_perfil` varchar(200) DEFAULT NULL,
+  `estado_perfil` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuario`
+--
+
+CREATE TABLE `tbl_usuario` (
+  `PK_id_usuario` varchar(25) NOT NULL,
+  `nombre_usuario` varchar(45) DEFAULT NULL,
+  `apellido_usuarios` varchar(45) DEFAULT NULL,
+  `password_usuario` varchar(45) DEFAULT NULL,
+  `cambio_contrasena` tinyint(4) DEFAULT NULL,
+  `estado_usuario` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tbl_usuario`
+--
+
+INSERT INTO `tbl_usuario` (`PK_id_usuario`, `nombre_usuario`, `apellido_usuarios`, `password_usuario`, `cambio_contrasena`,  `estado_usuario`) VALUES
+('MiUsuario', 'Usuario', 'Prueba', 'e19d5cd5af0378da05f63f891c7467af', 0, 1);
+
+--
+-- Disparadores `tbl_usuario`
+--
+DELIMITER $$
+CREATE TRIGGER `actualizarClave` BEFORE UPDATE ON `tbl_usuario` FOR EACH ROW IF NEW.password_usuario <> OLD.password_usuario THEN
+SET NEW.password_usuario = MD5(NEW.password_usuario);
+END IF
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `encriptarClave` BEFORE INSERT ON `tbl_usuario` FOR EACH ROW BEGIN
+SET NEW.password_usuario = MD5(NEW.password_usuario);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuario_aplicacion`
+--
+
+CREATE TABLE `tbl_usuario_aplicacion` (
+  `PK_id_usuario` varchar(25) NOT NULL,
+  `PK_id_aplicacion` int(11) NOT NULL,
+  `ingresar` tinyint(4) DEFAULT NULL,
+  `consultar` tinyint(4) DEFAULT NULL,
+  `modificar` tinyint(4) DEFAULT NULL,
+  `eliminar` tinyint(4) DEFAULT NULL,
+  `imprimir` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuario_perfil`
+--
+
+CREATE TABLE `tbl_usuario_perfil` (
+  `PK_id_usuario` varchar(25) NOT NULL,
+  `PK_id_perfil` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_aplicacion`
+--
+ALTER TABLE `tbl_aplicacion`
+  ADD PRIMARY KEY (`PK_id_aplicacion`,`PK_id_modulo`),
+  ADD KEY `fk_Aplicacion_Modulo` (`PK_id_modulo`);
+
+--
+-- Indices de la tabla `tbl_bitacora`
+--
+ALTER TABLE `tbl_bitacora`
+  ADD PRIMARY KEY (`PK_id_bitacora`,`PK_id_usuario`),
+  ADD KEY `fk_Bitacora_Usuario1` (`PK_id_usuario`);
+
+--
+-- Indices de la tabla `tbl_modulo`
+--
+ALTER TABLE `tbl_modulo`
+  ADD PRIMARY KEY (`PK_id_Modulo`);
+
+--
+-- Indices de la tabla `tbl_perfil_detalle`
+--
+ALTER TABLE `tbl_perfil_detalle`
+  ADD PRIMARY KEY (`PK_id_perfil`,`PK_id_aplicacion`),
+  ADD KEY `fk_Perfil_detalle_Aplicacion1` (`PK_id_aplicacion`);
+
+--
+-- Indices de la tabla `tbl_perfil_encabezado`
+--
+ALTER TABLE `tbl_perfil_encabezado`
+  ADD PRIMARY KEY (`PK_id_perfil`);
+
+--
+-- Indices de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  ADD PRIMARY KEY (`PK_id_usuario`);
+
+--
+-- Indices de la tabla `tbl_usuario_aplicacion`
+--
+ALTER TABLE `tbl_usuario_aplicacion`
+  ADD PRIMARY KEY (`PK_id_usuario`,`PK_id_aplicacion`),
+  ADD KEY `fk_tbl_usuario_aplicacion_tbl_aplicacion1` (`PK_id_aplicacion`);
+
+--
+-- Indices de la tabla `tbl_usuario_perfil`
+--
+ALTER TABLE `tbl_usuario_perfil`
+  ADD PRIMARY KEY (`PK_id_usuario`,`PK_id_perfil`),
+  ADD KEY `fk_Usuario_perfil_Perfil1` (`PK_id_perfil`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_bitacora`
+--
+ALTER TABLE `tbl_bitacora`
+  MODIFY `PK_id_bitacora` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_aplicacion`
+--
+ALTER TABLE `tbl_aplicacion`
+  ADD CONSTRAINT `fk_Aplicacion_Modulo` FOREIGN KEY (`PK_id_modulo`) REFERENCES `tbl_modulo` (`PK_id_Modulo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tbl_bitacora`
+--
+ALTER TABLE `tbl_bitacora`
+  ADD CONSTRAINT `fk_Bitacora_Usuario1` FOREIGN KEY (`PK_id_usuario`) REFERENCES `tbl_usuario` (`PK_id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tbl_perfil_detalle`
+--
+ALTER TABLE `tbl_perfil_detalle`
+  ADD CONSTRAINT `fk_Perfil_detalle_Aplicacion1` FOREIGN KEY (`PK_id_aplicacion`) REFERENCES `tbl_aplicacion` (`PK_id_aplicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Perfil_detalle_Perfil1` FOREIGN KEY (`PK_id_perfil`) REFERENCES `tbl_perfil_encabezado` (`PK_id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tbl_usuario_aplicacion`
+--
+ALTER TABLE `tbl_usuario_aplicacion`
+  ADD CONSTRAINT `fk_Usuario_aplicacion_Usuario1` FOREIGN KEY (`PK_id_usuario`) REFERENCES `tbl_usuario` (`PK_id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tbl_usuario_aplicacion_tbl_aplicacion1` FOREIGN KEY (`PK_id_aplicacion`) REFERENCES `tbl_aplicacion` (`PK_id_aplicacion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `tbl_usuario_perfil`
+--
+ALTER TABLE `tbl_usuario_perfil`
+  ADD CONSTRAINT `fk_Usuario_perfil_Perfil1` FOREIGN KEY (`PK_id_perfil`) REFERENCES `tbl_perfil_encabezado` (`PK_id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Usuario_perfil_Usuario1` FOREIGN KEY (`PK_id_usuario`) REFERENCES `tbl_usuario` (`PK_id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 
 --
 -- Dumping routines for database 'proyectogeneral'
